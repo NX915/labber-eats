3f93cdfac31da5ffb79858b6ba71d6ecad30c393
Sean Ni <nixiang915@hotmail.com>
2020-09-19 20:41:28 +0000

    add json return to /:id route

--- routes/orders.js
+++ routes/orders.js
@@ -12,8 +12,19 @@ module.exports = (db) => {
   const dbHelpers = require('../db/dbHelpers')(db);
 
   router.get("/", (req, res) => {
+    // {newOrders: [], pendingOrders: []}
     dbHelpers.getNewOrders()
       .then(orders => res.send(orders));
+  });
+
+  router.get("/:id", (req, res) => {
+    // {orderItems: [], orderDetail: []}
+    const output = {};
+    dbHelpers.getOrderDetails(req.params.id)
+      .then(res => output.orderDetails = res)
+      .then(() => dbHelpers.getItemsFromOrder(req.params.id))
+      .then(res => output.itemsFromOrder = res)
+      .then(() => res.send(output));
   });
 
   router.post("/:id", (req, res) => {
