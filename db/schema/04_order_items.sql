7910e72c8d221a02657990bb78eb160761d94bf9
Sean Ni <nixiang915@hotmail.com>
2020-09-19 15:41:06 +0000

    add orders route.js and mount it inside server.js

--- db/schema/04_order_items.sql
+++ db/schema/04_order_items.sql
@@ -1,9 +1,9 @@
 -- Drop and recreate order_items table
 
 DROP TABLE IF EXISTS order_items CASCADE;
-CREATE TABLE orders_items (
-  id SERIAL PRIMARY KEY NOT NULL,
+CREATE TABLE order_items (
   order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
   item_id INTEGER REFERENCES items(id) ON DELETE CASCADE,
-  quantity INTEGER NOT NULL DEFAULT 1
+  quantity INTEGER NOT NULL DEFAULT 1,
+  PRIMARY KEY (order_id, item_id)
 );
--- server.js
+++ server.js
@@ -35,11 +35,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const ordersRoutes = require("./routes/orders");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/orders", ordersRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
@@ -47,6 +49,10 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/control", (req, res) => {
   res.render("index");
 });
 
