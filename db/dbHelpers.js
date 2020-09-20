2959248c9b98191ab8b9e03774b0f107e63cbbe9
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-20 14:27:21 +0000

    Add some test conditions to addOrder. It checks if the user selected at least one item, if it informed valid name and phone. Pending: check if the order only contains available items

--- db/dbHelpers.js
+++ db/dbHelpers.js
@@ -70,44 +70,80 @@ module.exports = db => {
     order_items: create n rows where n is the number of distinct items. Should populate the row with the order_id, the item_id and quantity)
 
     expected input:
-    selectedItems = { item_id: quantity, item_id: quantity }
-    selectedItems = { 1: 3, 4: 1 }
-    userDetails = { name, phone }
-    we need to make sure that the selectedItems has at least one item selected (not an empty object)
-    we need to make sure that the user can only submit an order if both the name and phone fields are filled
+      {
+        selectedItems: { item_id: quantity, item_id: quantity },
+        userDetails: { name: 'name', phone: 'phone' }
+      }
+    example:
+      {
+        selectedItems: { 1:3, 3:5 },
+        userDetails: { name: 'Danilo', phone: '1234567890' }
+      }
+
  */
   const addOrder = obj => {
     const { selectedItems, userDetails } = obj;
+    // confirming that the user has selected at least one item (not an empty object)
+    if (Object.keys(selectedItems).length === 0) {
+      throw 'It seems that the user did not select any item'
+    }
+    // confirming that both the name and phone fields are filled
+    if (!userDetails.name) {
+      throw 'The name field does not contain a valid input'
+    }
+    if (!userDetails.phone || toString(userDetails.phone).length < 10) {
+      throw 'The phone number is either empty or incomplete'
+    }
 
-    // define userID and orderID variables to hold the ids of the new created rows
-    let userID;
-    let orderID;
     // query to insert a new user, returning its id
+    let userID;
     const newUserQuery = {
       text: `
-        INSERT INTO users (name, phone)
-        VALUES ($1, $2) RETURNING id;
+      INSERT INTO users (name, phone)
+      VALUES ($1, $2) RETURNING id;
       `,
       values: [userDetails.name, userDetails.phone]
     }
+
     // the query text to insert a new order, returning its id
+    let orderID;
     const newOrderQuery = `
       INSERT INTO orders (user_id)
       VALUES ($1) RETURNING id;
     `;
+
     // create a new let variable to hold the query text to create n rows in the order_items table, it will be updated with a for loop
     let assignItemsToOrderText = `INSERT INTO order_items (order_id, item_id, quantity)\nVALUES\n`;
+    // define a itemsID array to later check if all the requested item_id are available (will not check if it exists, because if it doesn't, the database will throw an error because it will violate foreign key constraint: item_id is not present in table items)
+    const itemsID = [];
     // create an array to hold the values to the new query. It will be populated with a for loop
     const assignItemsToOrderValues = []
     for (const itemID in selectedItems) {
+      itemsID.push(itemID);
+      // confirming that every item has a valid quantity
+      if (selectedItems[itemID] <= 0) {
+        throw 'It seems that not all of the selected quantities are valid'
+      }
       assignItemsToOrderText += `($1, $${assignItemsToOrderValues.length + 2}, $${assignItemsToOrderValues.length + 3}),\n`;
       assignItemsToOrderValues.push(itemID);
       assignItemsToOrderValues.push(selectedItems[itemID]);
     }
-    // delete the last row extra new line and comma and add the command to finish the query
+    // delete the last row extra new line and comma, and add the command to finish the query
     assignItemsToOrderText = assignItemsToOrderText.slice(0, -2) + ';';
-    // run the first query that will return the user_id
+
+    // create a new query to check if every item_id in the order is valid
+    console.log(itemsID);
+    // checkItemsQuery = {
+    //   text: `
+    //   SELECT id FROM items WHERE available = TRUE
+    //   `,
+    //   values: []
+    // }
+
+
+
     return db
+      // run the first query that will return the user_id
       .query(newUserQuery)
       .then(res => {
         userID = res.rows[0].id;
--- server.js
+++ server.js
@@ -34,11 +34,16 @@ const dbHelpers = require('./db/dbHelpers')(db);
 // dbHelpers.getItemsFromOrder(1)
 // .then(res => console.log('items from order:', res));
 
-// dbHelpers.addOrder({
-//   selectedItems: { 1:3, 3:5 },
-//   userDetails: { name: 'Danilo', phone: 1234567890 }
-// })
-// .then(res => res)
+dbHelpers.addOrder({
+  selectedItems: { 1:3, 3:5 },
+  userDetails: { name: 'Danilo', phone: 1234567890 }
+})
+.then(res => res)
+dbHelpers.addOrder({
+  selectedItems: { 1:3, 4:5 },
+  userDetails: { name: 'Bug tester', phone: 1234567890 }
+})
+.then(res => res)
 
 // dbHelpers.processOrder({order_id: 1}) // should be accepted
 // .then(res => res)
