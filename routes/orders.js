04459bd33fb93feba2328bfffb1050523f1a7652
Sean Ni <nixiang915@hotmail.com>
2020-09-20 12:04:40 +0000

    add dbhelper to decline function to order/id/decline route

--- routes/orders.js
+++ routes/orders.js
@@ -45,20 +45,30 @@ module.exports = (db) => {
 
   router.post("/:id", (req, res) => {
     const { id } = req.params;
+
     dbHelpers.processOrder({order_id: id})
       .then(() => {
-        res.send(`Successful POST to orders/:${id}`);
+        res.send(`Successful POST to orders/${id}`);
       })
       .catch((err) => {
-        res.send(`Unsuccessful POST to orders/:${id} ${err.message}`);
+        res.send(`Unsuccessful POST to orders/${id} ${err.message}`);
       });
     // console.log(`POST to orders/:${id}`);
     //test fail request
   });
 
   router.post("/:id/decline", (req, res) => {
-    res.send(`POST to orders/:${req.params.id}/decline`);
-    console.log(`POST to orders/:${req.params.id}/decline`);
+    const { id } = req.params;
+
+    dbHelpers.processOrder({order_id: id, accepted: false})
+      .then(() => {
+        res.send(`Successful POST to orders/${id}/decline`);
+      })
+      .catch((err) => {
+        res.send(`Unsuccessful POST to orders/${id}/decline ${err.message}`);
+      });
+    // res.send(`POST to orders/:${req.params.id}/decline`);
+    // console.log(`POST to orders/:${req.params.id}/decline`);
   });
 
   router.post("/:id/done", (req, res) => {
