ae01b64bb2ef5d39848937f3e8ffded00daee924
Sean Ni <nixiang915@hotmail.com>
2020-09-20 13:42:11 +0000

    add jquery button listener

--- public/scripts/control-buttons.js
+++ public/scripts/control-buttons.js
@@ -1,10 +1,17 @@
 const attachButtonListener = function() {
   $('ol').on('submit', (e) => {
     e.preventDefault();
+    const { action } = e.target;
+    // console.log(e);
+    // console.log(action);
+
+    return $.ajax({url: action, method: 'post'})
+      .then(res => console.log(res))
+      .catch(err => console.log('Server connection error ', err));
   });
 };
 
 //driver code
 $().ready(() => {
-  // attachButtonListener();
+  attachButtonListener();
 });
