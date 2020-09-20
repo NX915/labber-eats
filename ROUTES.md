93ead744564e43c33ffb3380702ff7c53e3d5e94
Fatima Nguyen <fatima3455@hotmail.com>
2020-09-20 02:56:12 +0000

    add quantity button

--- ROUTES.md
+++ ROUTES.md
@@ -2,11 +2,11 @@
 
 | HTTP method | URL pattern | Use |
 |---|---|---|
-| GET  | / | Shows full menu of restaurant |
-| GET  | /items | Return JSON with menu items |
-| POST | /orders | Submit order |
-| GET  | /control | See all orders (restaurant side) |
-| GET  | /orders | Return JSON with active orders (populates both columns) | 
-| POST  | /orders/:id |  confirm order |
-| POST  | /orders/:id/decline |  decline order|
-| POST  | /orders/:id/delete |  Archive orders that were fufilled |
+| GET   | /                   | Shows full menu of restaurant |
+| GET   | /items              | Return JSON with menu items |
+| POST  | /orders             | Submit order |
+| GET   | /control            | See all orders (restaurant side) |
+| GET   | /orders             | Return JSON with active orders (populates both columns) | 
+| POST  | /orders/:id         | confirm order |
+| POST  | /orders/:id/decline | decline order|
+| POST  | /orders/:id/delete  | Archive orders that were fufilled |
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,35 @@
-const renderItem = itemObj => {
+// const selectedItems = { item_id: quantity, };
+// const userDetails = { name: 'qleqe', phone: '12341839254'}
+// { selectedItems: {},
+let menuCache;
 
+const createItemElement = itemObj => {
+  const $item = `
+  <article class='menu-item' id=item_id_${itemObj.item_id}>
+    <div><img src=${itemObj.image_url} width="500"></div>
+    <div>
+        <h3>${itemObj.name}</h3>
+        <p>${itemObj.description}</p>
+        <p>$${itemObj.price / 100}</p>
+    <div>
+    <div>
+      <button class='dec button'>-</button>
+      <input type="text" name="${itemObj.item_id}" id="${itemObj.item_id}" value="0">
+      <button class='inc button'>+</button>
+    </div>
+  </article>
+  `;
+  return $item;
 };
+
+const renderMenu = arr => {
+  arr.forEach(item => $('main').append(createItemElement(item)));
+};
+
 $(document).ready(() => {
   $.ajax({url: '/items', method: 'get'})
     .then(res => {
-      for
+      menuCache = res;
+      renderMenu(res);
     });
 });
