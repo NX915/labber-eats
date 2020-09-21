6cc5eee8168ed7b9720650fedca42535113fb236
Sean Ni <nixiang915@hotmail.com>
2020-09-21 15:27:14 +0000

    comment out test code

--- public/scripts/control-buttons.js
+++ public/scripts/control-buttons.js
@@ -3,10 +3,10 @@ const attachButtonListener = function() {
     e.preventDefault();
     const { action, method, parentElement } = e.target;
     const userInput = {input: $(e.target).find('input.user_input').val()};
-    console.log(e);
+    // console.log(e);
     // let order_id = parentElement.id.split('_').pop();
     // order_id = order_id.pop();
-    console.log(userInput);
+    // console.log(userInput);
 
     $.ajax({url: action, method: method, data: userInput})
       .then(res => {
