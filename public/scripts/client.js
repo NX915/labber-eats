06db84b65f46650256f52edbcaad97dd619109a7
Fatima Nguyen <fatima3455@hotmail.com>
2020-09-21 12:41:35 +0000

    dynamically change subtotals

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,113 +1,6 @@
+/* eslint-disable no-undef */
 const selectedItems = {};
 let menuCache;
-// const { selectedItems: { itemId: 'quantity' }, userDetails:{ name: 'qleqe', phone: '12341839254'}}
-
-const createItemElement = (itemObj) => {
-  const $item = `
-  <article class='menu-item' id=${itemObj.id}>
-    <div><img src=${itemObj.image_url} width="500"></div>
-    <div>
-        <h3>${itemObj.name}</h3>
-        <p>${itemObj.description}</p>
-        <p>$${itemObj.price / 100}</p>
-    </div>
-    <div>
-      <button class='dec-button'>-</button>
-      <input type="number" name="quantity" value="0">
-      <button class='inc-button'>+</button>
-    </div>
-  </article>
-  `;
-  return $item;
-};
-
-const createCartItem = (itemObj, quant) => {
-  const $item = `
-  <article class='menu-item' id=${itemObj.id}>
-    <div><img src=${itemObj.image_url} width="500"></div>
-    <div>
-      <h3>${itemObj.name}</h3>
-    </div>
-    <div>
-      <button class='dec-button'>-</button>
-      <input type="number" name="quantity" value="${quant}">
-      <button class='inc-button'>+</button>
-    </div>
-    <div>
-      <p>$${itemObj.price * quant / 100}</p>
-    </div>
-  </article>
-  `;
-  return $item;
-};
-
-const renderMenu = arr => {
-  arr.forEach(item => $('main').append(createItemElement(item)));
-};
-
-const renderCart = (arr, cart) => {
-  for (const menuItem of arr) {
-    for (const itemId in cart) {
-      if (menuItem.id === parseInt(itemId)) {
-        $('main').append(createCartItem(menuItem, cart[itemId]));
-      }
-    }
-  }
-};
-
-const decreaseCounter = function(el) {
-  if (el.val() > 0) {
-    el.val(parseInt(el.val()) - 1);
-  }
-};
-
-const increaseCounter = function(el) {
-  el.val(parseInt(el.val()) + 1);
-};
-
-const removeFromCart = function(cart, id) {
-  if (cart[id]) {
-    if (cart[id] > 1) {
-      cart[id]--;
-    } else if (cart[id] === 1) {
-      delete cart[id];
-    }
-  }
-};
-
-const addToCart = function(cart, id) {
-  if (cart[id]) {
-    cart[id]++;
-  } else {
-    cart[id] = 1;
-  }
-};
-
-const updateCart = function(cart, id, value) {
-  if (value && value !== '0') {
-    cart[id] = parseInt(value);
-  } else {
-    if (cart[id]) {
-      delete cart[id];
-    }
-  }
-};
-
-const calculateTotal = (menu, itemObj) => {
-  let sum = 0;
-  for (const itemId in itemObj) {
-    const quantity = itemObj[itemId];
-
-    for (const menuItem of menu) {
-      if (menuItem.id === parseInt(itemId)) {
-        const price = menuItem.price;
-
-        sum += quantity * price;
-      }
-    }
-  }
-  return sum / 100;
-};
 
 $(document).ready(() => {
   $.ajax({url: '/items', method: 'get'})
@@ -144,62 +37,66 @@ $(document).ready(() => {
 
       // When client clicks on cart, repopulate page with only their selection
       $('#cart-btn').click(() => {
-        $('main').empty();
-        $('main').append('<h1>Cart</h1>');
-        renderCart(menuCache, selectedItems);
-        $('main').append(`
-        <div>
-          <h4>Total</h4>
-          <p>${calculateTotal(menuCache, selectedItems)}</p>
-        </div>
-        <form method='POST' action='/orders'>
-          <label for="name">Name:</label>
-          <input type="text" name="name" id="name" placeholder="Name">
-          <label for="phone-num">Phone number:</label>
-          <input type="text" name="phone" id="phone" placeholder="(xxx)xxx-xxxx">
-          <button type="submit">Submit Order</button>
-        </form>
-        `);
+        renderCartPage(menuCache, selectedItems);
 
-        // Decrease quantity when '-' clicked
+        // Decrease quantity when '-' clicked and updates subtotal + total
         $('.dec-button').click(function() {
           const clickedItemId = $(this).parent().parent().attr('id');
+          const $subTotalEl = $(this).parent().parent().find('.subtotal');
           let $counter = $(this).siblings('input');
+          const price = findPrice(clickedItemId, menuCache);
 
           decreaseCounter($counter);
           removeFromCart(selectedItems, clickedItemId);
+          updateSubtotal($subTotalEl, Number($counter.val()), price);
         });
 
-        // Increase quantity when '+' clicked
+        // Increase quantity when '+' clicked and updates subtotal + total
         $('.inc-button').click(function() {
           const clickedItemId = $(this).parent().parent().attr('id');
+          const $subTotalEl = $(this).parent().parent().find('.subtotal');
           let $counter = $(this).siblings('input');
+          const price = findPrice(clickedItemId, menuCache);
 
           increaseCounter($counter);
           addToCart(selectedItems, clickedItemId);
+          updateSubtotal($subTotalEl, Number($counter.val()), price);
         });
 
         // Update quantity when use types in the input field
         $('input[name$="quantity"]').on('input', function() {
           const itemId = $(this).parent().parent().attr('id');
+          const $subTotalEl = $(this).parent().parent().find('.subtotal');
           const inputValue = $(`#${itemId}`).find('input').val();
+          const price = findPrice(itemId, menuCache);
 
           updateCart(selectedItems, itemId, inputValue);
+          updateSubtotal($subTotalEl, Number(inputValue), price);
         });
 
+        // Add new order to database and shows confirmation page to client
         $('form').submit(function(event) {
           event.preventDefault();
           const name = $('#name').val();
           const phone = $('#phone').val();
-          const json = JSON.stringify({selectedItems, userDetails: {name, phone}});
+          const orderDetails = JSON.stringify({selectedItems, userDetails: {name, phone}});
 
-          $.ajax({
-            url: '/orders',
-            type: 'post',
-            dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
-            data: json});
-        })
+          submitOrder(orderDetails);
+          renderOrderConfirmation();
+
+          // submitOrderDetails(orderDetails)
+          //   .done((data) => console.log(data));
+
+          // const req = $.ajax({
+          //   url: '/orders',
+          //   type: 'post',
+          //   dataType: 'json',
+          //   contentType: 'application/json; charset=utf-8',
+          //   data: orderDetails});
+          // req.done(() => {
+          //   console.log('hello')
+
+        });
       });
     });
 });
