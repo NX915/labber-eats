119bd83494832d7206a5565c4f2e19d9dde97a1c
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-23 23:42:01 +0000

    Change map

--- public/scripts/client-helpers.js
+++ public/scripts/client-helpers.js
@@ -26,9 +26,9 @@ const createItemElement = (itemObj) => {
 };
 
 const createCategoryElement = (item) => {
-  const { category, category_id, category_description } = item;
+  const { category, category_description } = item;
   const element = `
-    <div id='category-${category_id}' class='category-container'>
+    <div id='${category}' class='category-container'>
       <h2>${category}</h2>
       <h3>${category_description}</h3>
     </div>`
@@ -39,13 +39,13 @@ const createCategoryElement = (item) => {
 const renderMenu = arr => {
   const addedCategories = {}
   arr.forEach(item => {
-    const { category, category_id } = item
+    const { category } = item
     if (addedCategories[category] === undefined) {
       addedCategories[category] = '';
       $('#menu-container').append(createCategoryElement(item))
-      $('nav').append(`<a href='#category-${category_id}'>${category}</a>`)
+      $('nav').append(`<a href='#${category}'>${category}</a>`)
     }
-    $(`#category-${category_id}`).append(createItemElement(item))
+    $(`#${category}`).append(createItemElement(item))
   });
 };
 
@@ -201,7 +201,7 @@ const renderOrderConfirmation = () => {
   <h1>Thank you for your order!</h1>
   <p>You will receive an SMS soon!</p>
   <div id='map_container'>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2795.152207143168!2d-73.59829188454364!3d45.52714253726242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc9197c8d8469ed%3A0x8269e006f5c4199b!2s5455%20Gaspe%20Ave%2C%20Montreal%2C%20QC%20H2T%203B3!5e0!3m2!1sen!2sca!4v1600902491898!5m2!1sen!2sca" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2795.1325196446114!2d-73.5986186845436!3d45.527538737235986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc91bac21dc4f8d%3A0x73e0460a68d1c265!2sLighthouse%20Labs!5e0!3m2!1sen!2sca!4v1600904458627!5m2!1sen!2sca" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
   </div`);
 };
 
